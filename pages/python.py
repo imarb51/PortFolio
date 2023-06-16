10aecf2f4901c61e8412f7c14d1c2cb51af6a591
imran <imranance99@gmail.com>
2023-06-16 21:07:47 +0530

    Intermediate==

--- pages/python.py
+++ pages/python.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(
-    page_title="pages",
-    layout="centered")
+st.set_page_config(layout="centered")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image("images/flutter.py.png")
+    st.image("images/python.png")
 with col2:
-    st.title("Flutter")
+    st.title("Python")
     content = """
-    This is the Page where I have developed 10+ Flutter Projects in all fields.
+    This is the Page where I have developed 15+ Python Projects in all fields.
     """
     st.info(content)
 
@@ -21,18 +19,15 @@ Below you can find the projects which I have developed or the projects where I h
 st.write(content2)
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
-df = pd.read_csv("flutter.csv", sep=";")
 
 with col3:
-    for index, row in df[:4].iterrows():
+    for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[4:].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
