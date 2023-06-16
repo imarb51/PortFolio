66ef5063a39e71993a575cd1565fd7f650d1cdec
imran <imranance99@gmail.com>
2023-06-16 21:36:07 +0530

    Final

--- pages/Machine.py
+++ pages/Machine.py
@@ -4,9 +4,9 @@ st.set_page_config(layout="centered")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image("images/photo.png")
+    st.image("images/machine.png")
 with col2:
-    st.title("Imran Shaikh")
+    st.title("Data Science and Machine Learning")
     content = """
     Hi, I am Imran Shaikh and I am Python, Java Developer as well as a Machine Learning Expert. I have also experience in Data Analysis and i have work on Tensorflow as well.
     I am graduating in 2024 and I love to learn new things and to participate in Competitions. I have also won Trophies in Football and Volleyball.
@@ -20,10 +20,10 @@ Below you can find the projects which I have developed or the projects where I h
 st.write(content2)
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
-df = pd.read_csv("data.csv", sep=";")
+df = pd.read_csv("python.csv", sep=";")
 
 with col3:
-    for index, row in df[:2].iterrows():
+    for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
@@ -31,8 +31,7 @@ with col3:
 
 
 with col4:
-    for index, row in df[2:].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-
--- pages/flutter.py
+++ pages/flutter.py
@@ -1,4 +1,37 @@
 import streamlit as st
 import pandas as pd
+st.set_page_config(layout="centered")
 
-st.header("Flutter")
+col1, col2 = st.columns(2)
+with col1:
+    st.image("images/flutter.png")
+with col2:
+    st.title("Flutter")
+    content = """
+    Hi, I am Imran Shaikh and I am Python, Java Developer as well as a Machine Learning Expert. I have also experience in Data Analysis and i have work on Tensorflow as well.
+    I am graduating in 2024 and I love to learn new things and to participate in Competitions. I have also won Trophies in Football and Volleyball.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find the projects which I have developed or the projects where I have added some features.
+"""
+
+st.write(content2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pd.read_csv("flutter.csv", sep=";")
+
+with col3:
+    for index, row in df[:4].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+
+
+
+with col4:
+    for index, row in df[4:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
